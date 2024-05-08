ab8889816f6fabb1ec8444f20f7efb41712f7a95
Verdraks <martinezorlando955@gmail.com>
2024-05-08 12:00:42 +0200

    push fucntion

--- Assets/Scripts/GameComponents/Entitys/Enemys/EnemySpawnerGestionary.cs
+++ Assets/Scripts/GameComponents/Entitys/Enemys/EnemySpawnerGestionary.cs
@@ -56,6 +56,8 @@ public class EnemySpawnerGestionary : MonoBehaviour
     }
     #endregion
 
+    public int GetWave() => currentWave;
+
     private void OnEnnemyDeath()
     {
         currentNbMonsterSpawn -= 1;
